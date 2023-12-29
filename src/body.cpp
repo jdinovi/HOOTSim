aad4eaf22f47ff2f933c6bab33fb7cc2f2764bb8
jdinovi <john@dinovi.net>
2023-12-29 18:55:09 -0500

    add tests for body implementation; slightly edit body.cpp implementation

--- src/body.cpp
+++ src/body.cpp
@@ -12,4 +12,4 @@ float DENSITY = 4E3;
 
 // Constructor definition
 Body::Body(const std::array<double, 3>* initial_position, const std::array<double, 3>* initial_velocity, double mass, float radius)
-    : Particle(initial_position, initial_velocity, mass), radius(radius == 0 ? pow(3 * mass / (4 * M_PI * DENSITY), 1/3) : radius) {};
+    : Particle(initial_position, initial_velocity, mass), radius((radius == 0) ? pow(3 * mass / (4 * M_PI * DENSITY), 1./3.) : radius) {};
